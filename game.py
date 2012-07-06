e6d00b993069ad1441ee92950a3e5629b5ffb7de
Juan Manuel García <jmg.utn@gmail.com>
2012-07-06 02:43:37 -0300

    check for horizontal and vertical words

--- game.py
+++ game.py
@@ -7,11 +7,7 @@ class Board(object):
 
     def is_valid_play(self, word):
 
-        return self.dictionary.is_valid_word(word) and self.is_valid_position(word)
-
-    def is_valid_position(self, word):
-
-        return True
+        return self.dictionary.is_valid_word(word) and word.is_valid_position()
 
 
 class Dictionary(object):
@@ -20,11 +16,7 @@ class Dictionary(object):
 
     def is_valid_word(self, word):
 
-        return self._list_to_string(word) in self._valid_words
-
-    def _list_to_string(self, word):
-
-        return "".join([unicode(char) for char in word])
+        return unicode(word) in self._valid_words
 
 
 class Tile(object):
@@ -39,6 +31,33 @@ class Tile(object):
         return self.char
 
 
+class Word(object):
+
+    def __init__(self, tiles):
+
+        self.tiles = tiles
+
+    def __unicode__(self):
+
+        return "".join([unicode(char) for char in self.tiles])
+
+    def is_valid_position(self):
+
+        return self.is_vertical() or self.is_horizontal()
+
+    def is_vertical(self):
+
+        return self._get_coord_values_set("y") == 1
+
+    def is_horizontal(self):
+
+        return self._get_coord_values_set("x") == 1
+
+    def _get_coord_values_set(self, coord):
+
+        return len(set([getattr(tile, coord) for tile in self.tiles]))
+
+
 if '__main__' == __name__:
 
     import unittest
@@ -49,9 +68,19 @@ if '__main__' == __name__:
 
             self.board = Board((10,10))
 
-        def test_first_word(self):
+        def test_valid_word(self):
 
-            word = [Tile("O", (0,0)), Tile("F", (1,0))]
+            word = Word([Tile("O", (0,0)), Tile("F", (1,0))])
             self.assertTrue(self.board.is_valid_play(word))
+
+        def test_invalid_word(self):
+
+            word = Word([Tile("A", (0,0)), Tile("S", (1,0)), Tile("F", (2,0)), Tile("G", (3,0))])
+            self.assertFalse(self.board.is_valid_play(word))
+
+        def test_invalid_word_position(self):
+
+            word = Word([Tile("O", (0,0)), Tile("F", (1,1))])
+            self.assertFalse(self.board.is_valid_play(word))
 
     unittest.main()
